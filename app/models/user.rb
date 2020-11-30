81fb1fd544a7e04337b5f31776367195216a2077
satoshi0takahashi <v.c.2000@icloud.com>
2020-11-30 16:50:11 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_katakana, presence: true
-         validates :last_name_katakana, presence: true
+         validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+         validates :last_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+         validates :first_name_katakana, presence: true, format: { with: /\A(?:\p{Katakana})+\z/}
+         validates :last_name_katakana, presence: true, format: { with: /\A(?:\p{Katakana})+\z/}
          validates :birth, presence: true
-
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+         validates :password, format: { with: VALID_PASSWORD_REGEX}, length: { minimum: 6 }
 end
