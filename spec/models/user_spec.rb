5fbf1d925f701d5597e26da43b238c7a6c87c760
satoshi0takahashi <v.c.2000@icloud.com>
2020-11-30 16:23:07 +0900

    ユーザー登録機能

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      end
+      it "nicknameが6文字以下で登録できる" do
+      end
+      it "passwordが6文字以上であれば登録できる" do
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+      end
+      it "nicknameが7文字以上であれば登録できない" do
+      end
+      it "emailが空では登録できない" do
+      end
+      it "重複したemailが存在する場合登録できない" do
+      end
+      it "passwordが空では登録できない" do
+      end
+      it "passwordが5文字以下であれば登録できない" do
+      end
+      it "passwordが存在してもpassword_confirmationが空では登録できない" do
+      end
+    end
+  end
+end
