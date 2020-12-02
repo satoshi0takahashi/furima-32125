34f6500226d31683b93efed4294b2b6aedaaf610
satoshi0takahashi <v.c.2000@icloud.com>
2020-12-02 11:33:48 +0900

    バリデーションまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :last_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :first_name_katakana, presence: true, format: { with: /\A(?:\p{Katakana})+\z/ }
-  validates :last_name_katakana, presence: true, format: { with: /\A(?:\p{Katakana})+\z/ }
   validates :birth, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: PASSWORD_REGEX }
-  validates :password_confirmation, presence: :true, format: { with: PASSWORD_REGEX }
+  with_options fformat: { with: /\A(?:\p{Katakana})+\z/ } do
+    validates :first_name_katakana, presence: true
+    validates :last_name_katakana, presence: true
+  end
+
+  with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i} do
+    validates :password
+    validates :password_confirmation, presence: :true
+  end
 end
