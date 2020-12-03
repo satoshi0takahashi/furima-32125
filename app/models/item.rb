5ffe04a4bd7361b16f9bc3205328ff6f54c4aa2a
satoshi0takahashi <v.c.2000@icloud.com>
2020-12-03 12:18:59 +0900

    販売価格表示実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :detail, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, inclusion: {in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
