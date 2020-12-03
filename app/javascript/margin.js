80f3b1fa25d13f3a8029c2b7e727d65015a5430a
satoshi0takahashi <v.c.2000@icloud.com>
2020-12-03 21:01:34 +0900

    手数料表示変更

--- app/javascript/margin.js
+++ app/javascript/margin.js
@@ -2,7 +2,7 @@ function margin(){
   var item = document.getElementById("item-price");
   var add = document.getElementById("add-tax-price");
   var total = document.getElementById("profit");
-  item.addEventListener("keydown", function(){
+  item.addEventListener("input", function(){
   var items = item.value;
   var calc = items * 0.1;
   var prof = items - (items * 0.1);
