a331c8e122869f1e7e281c1167e2c08e2bde0a8a
satoshi0takahashi <v.c.2000@icloud.com>
2020-12-09 18:56:07 +0900

    バリデーション追加

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -2,10 +2,10 @@ class UserOrder
   include ActiveModel::Model
   attr_accessor :postal_code, :shipping_area_id, :city, :address, :building, :phone_number, :token, :user_id, :item_id, :order_id
 
-  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
   validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A[0-9]+\z/ }, length: {maximum: 11}
