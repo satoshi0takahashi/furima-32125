216d0a116271c291da648fef23ae6c2cbdbfec73
satoshi0takahashi <v.c.2000@icloud.com>
2020-12-10 11:26:25 +0900

    edit遷移変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id && @item.order.present?
+    if @item.order.present? && @item.user_id == current_user.id 
       redirect_to root_path
     elsif @item.user_id != current_user.id
       redirect_to :index
